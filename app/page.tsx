ba39c6c9c5cbf590bc520e71c0814d4a2ba95049
inesjusto <ines.justo08@gmail.com>
2026-01-03 16:13:57 +0000

    fix: corrigidos erros de build (eslint + pages)

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <section className="text-white p-10">
       <h1 className="text-4xl font-bold text-[#1ecb73]">
-        
+        Homepage
       </h1>
     </section>
   )
